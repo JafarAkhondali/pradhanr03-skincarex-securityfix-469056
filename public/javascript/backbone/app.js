b5335392cdc2f698c6bf9615738f0a0822693d78
Rubina Pradhan <pradhanr03@gmail.com>
2015-09-14 21:44:06 -0400

    socket

--- public/javascript/backbone/app.js
+++ public/javascript/backbone/app.js
@@ -247,8 +247,8 @@ var chatPage = function() {
 
      var unk = 'Anonymous';
      var name,
-            socket = io.connect("http://localhost:3000");
-            // socket = io.connect();
+            // socket = io.connect("http://localhost:3000");
+            socket = io.connect();
 
         // $(function () {
             //as the user to enter their nick name or name.
